82e4d4d406810f38e3e316e9c824335762d70682
Jeffrey <jeffrey.s.wu@sjsu.edu>
2020-10-17 05:16:30 -0700

    Added delete, duplicate functionality to cart page. Cart page also should update according to changes immediately. Primary key generation method added to Cart page.

--- src/views/Cart/Cart.js
+++ src/views/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 import Cookies from 'js-cookie';
@@ -6,9 +6,10 @@ import Cookies from 'js-cookie';
 
 function Cart() {
 
-  //var [products, setProducts] = useState(1);
+  var [products, setProducts] = useState(1);
 
-  let products = JSON.parse(Cookies.get('sugar'));
+  products = JSON.parse(Cookies.get('sugar'));
+  generatePrimaryKeys();
 
   let selectedRows = [];
 
@@ -17,7 +18,7 @@ function Cart() {
     clickToSelect: true,
     bgColor: 'gray',
     onSelect: (row, isSelect, rowIndex, e) => {
-      window.alert("[" + row + ", " + isSelect + ", " + rowIndex + ", " + e + "]");
+      //window.alert("[" + row + ", " + isSelect + ", " + rowIndex + ", " + e + "]");
       if(isSelect){
         console.log("Added " + rowIndex);
         selectedRows.push(rowIndex);
@@ -30,10 +31,15 @@ function Cart() {
         }
       }
       console.log(selectedRows);
-    }
+    },
+    selected: []
   };
 
   const columns = [{
+    dataField: 'id',
+    text: 'ID',
+    hidden: false
+  }, {
     dataField: 'name',
     text: 'Name'
   }, {
@@ -53,17 +59,38 @@ function Cart() {
     text: 'Type'
   }];
 
+
   function duplicateRow(){
     console.log(selectedRows);
     for(var i = 0; i < selectedRows.length; i++){
       products.push(products[selectedRows[i]]);
       console.log("Duplicating: " + selectedRows[i]);
     }
+    generatePrimaryKeys();
+    Cookies.set('sugar', products);
+    setProducts(JSON.parse(Cookies.get('sugar')));
     console.log(products);
-    //var products2 = products;
-    //setProducts(products2)
+  }
+
+  function deleteRow(){
+    console.log(selectedRows);
+    selectedRows.sort();
+    for(var i = selectedRows.length - 1; i >= 0; i--){
+      products.splice(selectedRows[i], 1)
+      console.log("Deleting: " + selectedRows[i]);
+    }
+    generatePrimaryKeys();
+    Cookies.set('sugar', products);
+    setProducts(JSON.parse(Cookies.get('sugar')));
+    console.log(products);
   }
   
+  function generatePrimaryKeys(){
+    for(var i = 0; i < products.length; i++){
+      products[i].id = i;
+    }
+  }
+
   return(
     <div class="m-5">
       <Button href="/Add" variant="dark">Back</Button>{' '}
@@ -71,14 +98,13 @@ function Cart() {
       <BootstrapTable
         wrapperClasses="table-responsive"
         striped bordered
-        keyField='name'
+        keyField='id'
         onDataSizeChange= {(e) => {console.log(e)}}
         data={ products }
         columns={ columns }
         selectRow={ selectRow }
       />
-      <cartTable/>
-      <Button variant="dark">Delete</Button>{' '}
+      <Button variant="dark" onClick={deleteRow}>Delete</Button>{' '}
       <Button variant="dark" onClick={duplicateRow}>Duplicate</Button>{' '}
       <Button variant="dark">Examine</Button>{' '}
       <Button variant="dark">Save</Button>{' '}
